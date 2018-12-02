7024a794c73bb9f2214770d7267610356ee7d5be
ced17i026 <ced17i026@iiitdm.ac.in>
2018-12-02 14:57:32 +0530

    Fetching data from database and display to browser

--- first_app/views.py
+++ first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,webpage,AccessRecord
 # Create your views here.
 def index(req):
-    my_dict = {'insert_me': 'Hello, I made this page'}
+    weblist = AccessRecord.objects.order_by('date')
+    my_dict = {'access_record':weblist}
     return render(req,'index.html',context=my_dict)
 def help(req):
     vpas = {'help': 'This has been redirect form help page'}
