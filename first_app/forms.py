84145ccc3d12df8d78d0bde406b0e48001386506
ced17i026 <ced17i026@iiitdm.ac.in>
2018-12-03 19:12:39 +0530

    Add importing other html page to another html page in app, major change in index.html,base.html

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from django.core import validators
 class formName(forms.Form):
     name=forms.CharField()
     email=forms.CharField()
+    email_again=forms.CharField(label='Please enter your Email again')
     text=forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['email_again']
+        if email != vemail:
+            raise forms.ValidationError("Make sure your Email matches")
